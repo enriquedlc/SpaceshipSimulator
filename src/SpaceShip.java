efe4acaf80d15abe0aa8d8a2e72beb590b145745
enriquedlc <enriquedlc2d@gmail.com>
2022-04-19 21:32:10 +0100

    Refactor: cambiado el tipo de variable de aceleracion, velocidadX y velocidadY
    Se han cambiado los tipos de variables porque al tener un valor float y uno int no es posible hacer la suma en el método speedUp, por lo tanto ahora estas variables serán de tipo int. También se ha cambiado en el constructor (aceleracion).

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -2,9 +2,9 @@ public class SpaceShip {
 
     private String nombre;
     private String matriculaGalactica;
-    private float aceleracion;
-    private float velocidadX = 0;
-    private float velocidadY = 0;
+    private int aceleracion;
+    private int velocidadX = 0;
+    private int velocidadY = 0;
     private int coordenadaX = 0;
     private int coordenadaY = 0;
     private int direccionX = 0;
@@ -12,82 +12,10 @@ public class SpaceShip {
 
     public SpaceShip(String nombre,
                      String matriculaGalactica,
-                     float aceleracion) {
+                     int aceleracion) {
 
         this.nombre = nombre;
         this.matriculaGalactica = matriculaGalactica;
         this.aceleracion = aceleracion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getMatriculaGalactica() {
-        return matriculaGalactica;
-    }
-
-    public void setMatriculaGalactica(String matriculaGalactica) {
-        this.matriculaGalactica = matriculaGalactica;
-    }
-
-    public float getAceleracion() {
-        return aceleracion;
-    }
-
-    public void setAceleracion(float aceleracion) {
-        this.aceleracion = aceleracion;
-    }
-
-    public float getVelocidadX() {
-        return velocidadX;
-    }
-
-    public void setVelocidadX(float velocidadX) {
-        this.velocidadX = velocidadX;
-    }
-
-    public float getVelocidadY() {
-        return velocidadY;
-    }
-
-    public void setVelocidadY(float velocidadY) {
-        this.velocidadY = velocidadY;
-    }
-
-    public int getCoordenadaX() {
-        return coordenadaX;
-    }
-
-    public void setCoordenadaX(int coordenadaX) {
-        this.coordenadaX = coordenadaX;
-    }
-
-    public int getCoordenadaY() {
-        return coordenadaY;
-    }
-
-    public void setCoordenadaY(int coordenadaY) {
-        this.coordenadaY = coordenadaY;
-    }
-
-    public int getDireccionX() {
-        return direccionX;
-    }
-
-    public void setDireccionX(int direccionX) {
-        this.direccionX = direccionX;
-    }
-
-    public int getDireccionY() {
-        return direccionY;
-    }
-
-    public void setDireccionY(int direccionY) {
-        this.direccionY = direccionY;
     }
 }

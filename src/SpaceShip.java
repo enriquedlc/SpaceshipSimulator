87c3ad53211357c0e33a0849447fb96929b43459
enriquedlc <enriquedlc2d@gmail.com>
2022-04-19 11:17:39 +0100

    Feature: añadido getter and setter para los atributos de la clase SpaceShip.java.

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -18,4 +18,76 @@ public class SpaceShip {
         this.matriculaGalactica = matriculaGalactica;
         this.aceleracion = aceleracion;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMatriculaGalactica() {
+        return matriculaGalactica;
+    }
+
+    public void setMatriculaGalactica(String matriculaGalactica) {
+        this.matriculaGalactica = matriculaGalactica;
+    }
+
+    public float getAceleracion() {
+        return aceleracion;
+    }
+
+    public void setAceleracion(float aceleracion) {
+        this.aceleracion = aceleracion;
+    }
+
+    public float getVelocidadX() {
+        return velocidadX;
+    }
+
+    public void setVelocidadX(float velocidadX) {
+        this.velocidadX = velocidadX;
+    }
+
+    public float getVelocidadY() {
+        return velocidadY;
+    }
+
+    public void setVelocidadY(float velocidadY) {
+        this.velocidadY = velocidadY;
+    }
+
+    public int getCoordenadaX() {
+        return coordenadaX;
+    }
+
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+
+    public int getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+
+    public int getDireccionX() {
+        return direccionX;
+    }
+
+    public void setDireccionX(int direccionX) {
+        this.direccionX = direccionX;
+    }
+
+    public int getDireccionY() {
+        return direccionY;
+    }
+
+    public void setDireccionY(int direccionY) {
+        this.direccionY = direccionY;
+    }
 }
